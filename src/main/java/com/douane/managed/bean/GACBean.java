8d7ddfc3666f992d4bb56ebc9231900145abbfa6
nixiton <nyaina.andriambolamalala@gmail.com>
2017-11-16 00:03:59 +0100

    nix 28

--- src/main/java/com/douane/managed/bean/GACBean.java
+++ src/main/java/com/douane/managed/bean/GACBean.java
@@ -71,7 +71,7 @@ public class GACBean {
     {
         //usermetierimpl.entrerMateriel(op);
         usermetierimpl.entrerMateriel(this.getCurentOperation());
-        
+        this.setCurentOperation(null);
     }
 
     public void validateAttributionDetenteur(OpAttribution attr)
