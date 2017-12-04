b695e2eb15c831c6e7021b43eb9175abfcb7ed9d
nixiton <ny.andriambolamalala@gmail.com>
2017-12-04 17:06:01 +0100

    nix 0112

--- src/main/java/com/douane/managed/bean/GACBean.java
+++ src/main/java/com/douane/managed/bean/GACBean.java
@@ -381,8 +381,8 @@ public class GACBean {
 
         this.setListMaterielByDet(usermetierimpl.getListMatByDet(((OpAttribution)operation).getMat().getDetenteur()));
 
-        /*ListIterator<Materiel> it = this.listMaterielByDet.listIterator();
-          while(it.hasNext()){
+        ListIterator<Materiel> it = this.getListMaterielByDet().listIterator();
+          /*while(it.hasNext()){
              setTotal(this.total+(Float)(it.next().getPu()));
           }*/
 
