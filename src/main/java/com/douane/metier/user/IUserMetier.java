42ad16a333582fbdfab1f72c6522b969a7fc6c56
tex31 <nyhoavy_nomena@yahoo.ca>
2018-01-10 21:01:25 +0300

    okay

--- src/main/java/com/douane/metier/user/IUserMetier.java
+++ src/main/java/com/douane/metier/user/IUserMetier.java
@@ -162,15 +162,17 @@ public interface IUserMetier {
 	public List<Article> getListArticleByDetenteurByValida(boolean valide, Agent detenteur);
 	public List<Article> getListArticleByValidationByDirection(boolean valide, Direction d);
 	
-	public ArticleNouv addArticleNouv(CodeArticle cde, Agent ben, Agent depo, Fournisseur fourn, Float prix, Long nombre, Marque marqueArt,String caraArt);
-	public ArticleEx addArticleEx(CodeArticle cde, Agent ben, Agent depo, Float prix, Long nombre, Marque marqueArt,String caraArt);
-	
 	
 	public List<OpEntreeArticle> getListOpEntreeArtByValideByDirection(EtatOperation etat, Direction direction, Date startDate, Date endDate);
 	
 	public List<OpSortieArticle> getListOpSortieArtByValideByDirection(EtatOperation etat,Direction direction, Date startDate, Date endDate);
 
+	
 
+	public ArticleNouv addArticleNouv(CodeArticle cde, Agent ben, Agent depo, Fournisseur fourn, Float prix, Long nombre, Marque marqueArt,String caraArt);
+	public ArticleEx addArticleEx(CodeArticle cde, Agent ben, Agent depo, Float prix, Long nombre, Marque marqueArt,String caraArt);
+	
+	
 	public OpSortie reqSortirMateriel(Materiel m, MotifSortie motif, Direction d, Agent oper)throws Exception;;
 	public OpSortie reqSortirMateriel(Materiel m, MotifSortie motif, Agent oper)throws Exception;;
 
