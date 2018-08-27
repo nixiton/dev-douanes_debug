9cc1427cacaec62a99bac609434df2dc9f5ec722
tex31 <nyhoavy_nomena@yahoo.ca>
2018-08-27 15:17:20 +0300

    initial data

---
+++ src/main/resources/META-INF/data/V1.3__fill_data.sql
@@ -0,0 +1,15 @@
+--
+-- TOC entry 2157 (class 0 OID 323781)
+-- Dependencies: 176
+-- Data for Name: agent; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+INSERT INTO agent VALUES (1, true, 999999, 'Administrateur', '$2a$10$nVMDUP0w6Wji4FakfFNiBe2feKLK/oo4b9iFcKjXtqysCAAxN2/Py', NULL, NULL, NULL, NULL, 1, NULL);
+
+--
+-- TOC entry 2195 (class 0 OID 323945)
+-- Dependencies: 214
+-- Data for Name: useri; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+INSERT INTO useri VALUES (1, 'Administrateur', 'ROLE_ADMIN');
